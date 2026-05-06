231f200b1b117451a2ba071c4bbfc955de1c8493
Leo Galla <leonelgallaretto@hotmail.com>
2026-05-06 16:26:30 -0300

    ListaMovimientos

--- BilleteraVirtualUAI/Grupo1.py
+++ BilleteraVirtualUAI/Grupo1.py
@@ -1,4 +1,5 @@
 
+historial = []
 nombre = input("Ingrese su nombre: ")
 mail = input("Ingrese su Correo: ")
 
@@ -18,17 +19,34 @@ def depositar(saldo):
         saldo+=cantidad
     else: 
         print("Saldo Invalido")
+    movimiento={
+        "Tipo": "Deposito",
+        "Monto": cantidad   
+        }
+    historial.append(movimiento)
     print(f"Su saldo actual es {saldo}")
     return saldo
-
+#Grupo 4 
 def extraer(saldo):
     cantidad = float((input("Ingresa la cantidad a Extraer: $")))
     if cantidad > 0 and cantidad <= saldo:
         saldo-=cantidad
     else: 
         print("Saldo insuficiente")
+    movimiento={
+        "Tipo": "Extraccion",
+        "Monto": cantidad   
+        }
+    historial.append(movimiento)
     print(f"Su saldo actual es {saldo}")
     return saldo
+#Grupo 5
+def verHistorial() :
+    for movimientos in historial:
+        print(f"{movimientos['Tipo']}: ${movimientos['Monto']:.2f}")
+    print("-" * 25)
+    print(f"SALDO ACTUAL: ${saldo:.2f}\n")
+
 # Grupo 2
 saldo = 0.0;
 while True:
@@ -39,7 +57,7 @@ while True:
         case "2":
             saldo = extraer(saldo)
         case "3":
-            print("")
+            verHistorial()
         case "4":
             break   
         case _:
